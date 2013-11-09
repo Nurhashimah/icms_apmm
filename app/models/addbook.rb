3a223f9b24dc6e659344cadf0ce22899d2b57c01
Nurhashimah <Shimah@MacBook-2.local>
2013-11-09 14:16:50 +0800

    Fixed-some items unable to be printed(office supplies-PRINT-line 65) + minor changes on SHOW & EDIT page

--- app/models/addbook.rb
+++ app/models/addbook.rb
@@ -3,5 +3,5 @@ class Addbook < ActiveRecord::Base
   has_many :manufacturers, :class_name => 'Asset', :foreign_key => 'manufacturer_id'
   has_many :books,  :foreign_key => 'supplier_id'
   has_many :courses, :class_name => 'Ptcourse', :foreign_key => 'provider'
-  has_many :addsupp, :class_name => 'Addsupplier', :foreign_key => 'addressbook_id'
+  has_many :addsupp, :class_name => 'Addsupplier', :foreign_key => 'addressbook_id', :dependent => :nullify
 end
