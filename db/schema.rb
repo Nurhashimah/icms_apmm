258129e6375e643ff21ae26165d183d019b83bbc
Nurhashimah <Shimah@MacBook-2.local>
2013-11-26 00:55:49 +0800

    Fixed - lecturer appraisal(different student) & fixes for Data Analysis and Average Score of Lecturers ie INDEX, ADD, EDIT, SHOW & PRINT pg

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131122103038) do
+ActiveRecord::Schema.define(:version => 20131125103510) do
 
   create_table "accessions", :force => true do |t|
     t.integer  "book_id"
@@ -731,6 +731,7 @@ ActiveRecord::Schema.define(:version => 20131122103038) do
     t.string   "invite_lec"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "average_lecturer_id"
   end
 
   create_table "events", :force => true do |t|
